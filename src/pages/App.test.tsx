22196763a4f1305fa1c5ae6962b682a16ec15448
boyang <wangboyang1991@gmail.com>
2023-07-20 19:48:45 +0200

    A second testcase done. Turns out, to mock element that uses data API, you do need to provide routes and router, but not the element. no choice

---
+++ src/pages/App.test.tsx
@@ -0,0 +1,35 @@
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from 'src/app/store';
+import React from 'react';
+import App from 'src/pages/App';
+import { MemoryRouter, RouterProvider, createMemoryRouter } from 'react-router-dom';
+
+test('renders Diary title', () => {
+  jest.mock('react-router-dom', () => ({
+    ...(jest.requireActual('react-router-dom') as any),
+    useMatches: jest.fn().mockReturnValue([{ pathname: '/entry11' }]),
+    useLocation: jest.fn().mockReturnValue({ pathname: '/entry11' }),
+  }));
+
+  const routes = [
+    {
+      path: '/entry',
+      element: <App />,
+    },
+  ];
+  const router = createMemoryRouter(routes, {
+    initialEntries: ['/entry'],
+  });
+
+  render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </React.StrictMode>,
+  );
+
+  expect(screen.getByText(/Not logged in/i)).toBeInTheDocument();
+});
