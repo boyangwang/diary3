53410f6bd5801e989717e73413f76ab994b31e60
yusixian <3450414733@qq.com>
2023-07-31 17:10:55 +0800

    feat: doned entry list

--- src/components/entry/EntryInstanceList.tsx
+++ src/components/entry/EntryInstanceList.tsx
@@ -1,7 +1,7 @@
 import { Button, List } from 'antd';
 import { deleteEntryInstance } from '../../app/entry-instances-slice';
 import { useAppDispatch } from '../../app/store';
-import { EntryInstance } from '../../app/types-constants';
+import { EntryInstance, formatDate } from '../../app/types-constants';
 
 const EntryInstanceCardDeleteButton = (props: { entryInstance: EntryInstance }) => {
   const dispatch = useAppDispatch();
@@ -18,18 +18,58 @@ const EntryInstanceCardDeleteButton = (props: { entryInstance: EntryInstance }) 
   );
 };
 
-const EntryInstanceList = (props: { entryInstancesArray: EntryInstance[] }) => (
-  <List
-    dataSource={props.entryInstancesArray}
-    renderItem={(item: EntryInstance) => (
-      <List.Item key={item.id} actions={[<EntryInstanceCardDeleteButton key={item.id} entryInstance={item} />]}>
-        <List.Item.Meta avatar={item.points} title={item.entryTypeId} description={item.notes} />
-        <span>id: {item.id}</span>
-        <span>createdAt: {item.createdAt}</span>
-        <span>updatedAt: {item.updatedAt}</span>
-      </List.Item>
-    )}
-  />
-);
+const EntryInstanceList = (props: { entryInstancesArray: EntryInstance[] }) => {
+  const { entryInstancesArray } = props;
+  console.log({ entryInstancesArray });
+  return (
+    <div className="flex flex-wrap justify-center gap-3">
+      {entryInstancesArray?.length
+        ? entryInstancesArray.map((item) => {
+            const { id, points, notes, entryTypeId, createdAt, updatedAt } = item;
+            return (
+              <div className="flex flex-col gap-2 rounded-lg border border-black/50 p-2" key={id}>
+                <div className="flex items-center gap-4">
+                  ID:
+                  <div className="flex-grow"> {id}</div>
+                </div>
+                <div className="flex items-center gap-4">
+                  entryTypeId:
+                  <div className="flex-grow"> {entryTypeId}</div>
+                </div>
+                <div className="flex items-center gap-4">
+                  points:
+                  <div className="flex-grow"> {points}</div>
+                </div>
+                <div className="flex items-center gap-4">
+                  notes:
+                  <div className="flex-grow"> {notes || <span className="text-gray-400">Empty</span>}</div>
+                </div>
+                <div className="flex items-center gap-4">
+                  createdAt:
+                  <div className="flex-grow">{formatDate(createdAt)}</div>
+                </div>
+                <div className="flex items-center gap-4">
+                  updatedAt:
+                  <div className="flex-grow">{formatDate(updatedAt)}</div>
+                </div>
+                <EntryInstanceCardDeleteButton key={id} entryInstance={item} />
+              </div>
+            );
+          })
+        : null}
+    </div>
+    // <List
+    //   dataSource={props.entryInstancesArray}
+    //   renderItem={(item: EntryInstance) => (
+    //     <List.Item key={item.id} actions={[<EntryInstanceCardDeleteButton key={item.id} entryInstance={item} />]}>
+    //       <List.Item.Meta avatar={item.points} title={item.entryTypeId} description={item.notes} />
+    //       <span>id: {item.id}</span>
+    //       {/* <span>createdAt: {item.createdAt}</span>
+    //       <span>updatedAt: {item.updatedAt}</span> */}
+    //     </List.Item>
+    //   )}
+    // />
+  );
+};
 
 export default EntryInstanceList;
--- src/components/entry/EntryProgressBar.tsx
+++ src/components/entry/EntryProgressBar.tsx
@@ -2,8 +2,9 @@ import { Progress } from 'antd';
 import './EntryProgressBar.css';
 
 function EntryProgressBar(props: { points: number }) {
-  const points = props.points;
+  const points = props.points + 10;
   const percent = Math.ceil((points * 10000) / 24) / 100;
+  const current = (percent / 100) * 24;
   const gradient = {
     '0%': '#3385E4',
     '25%': '#6CD261',
@@ -11,9 +12,24 @@ function EntryProgressBar(props: { points: number }) {
     '75%': '#EA7E30',
     '100%': '#924FDA',
   };
+  console.log(percent);
   return (
-    <div className="diary-entry-progress-bar">
-      <Progress percent={percent} status="active" strokeColor={gradient} format={(percent) => props.points} />
+    <div className="relative flex w-full gap-2">
+      <div className="absolute left-0 top-[0.4rem] z-[1] flex flex-col items-center">
+        <i className="block rounded-full border-2 border-white bg-transparent p-1" />0
+      </div>
+      <div className="absolute left-1/3 top-[0.4rem] z-[1] flex flex-col items-center">
+        <i className="block rounded-full border-2 border-white bg-transparent p-1" />8
+      </div>
+      <div className="absolute left-2/3 top-[0.4rem] z-[1] flex flex-col items-center">
+        <i className="block rounded-full border-2 border-white bg-transparent p-1" />
+        16
+      </div>
+      <div className="absolute right-0.5 top-[0.4rem] z-[1] flex flex-col items-center">
+        <i className="block rounded-full border-2 border-white bg-transparent p-1" />
+        24
+      </div>
+      <Progress className="flex-grow" status="active" strokeColor={gradient} showInfo={false} percent={percent} />
     </div>
   );
 }
